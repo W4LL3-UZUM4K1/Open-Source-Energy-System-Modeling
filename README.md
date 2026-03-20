9fb4b004bc33894aeab444879ac558a48153b351
W4LL3-UZUM4K1 <mrius.myr@gmail.com>
2026-03-20 16:04:53 +0100

    Add main.py, tests, requirements and workflow

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Open-Source-Energy-System-Modeling
-The present repository contains all relevant files and information for the course "Open Source Energy Systems Modeling" for the summer semester 2026.
+The present repository contains all relevant files regarding the first Homework for the course "Open Source Energy Systems Modeling" for the summer semester 2026.
+In this Project the aim was it to get us familiar with github and its key functions clone, push, pull. Also some functions were to be implemented and tests formulated for them.
---
+++ main.py
@@ -0,0 +1,32 @@
+def hydro_power(n, q, h):
+    """Function to calculate the possible power
+    of a hydroelectric power plant depending on
+    the inputs efficiency (n), the flow rate (q),
+    and the drop height(h)"""
+    g = 9.81 #Gravity in m/S
+    P = g * n * q * h #Calculates Power in W
+    return P
+
+def pv_power(P_STC, G, T_module, beta):
+    """
+    Function to calculate the PV power under real-world conditions depending
+    on the nominal power of the module under standard test conditions (P_STC),
+    current solar irradiance (G), module temperature (T_module) and
+    the temperature coefficient (beta)."""
+    G_STC = 1000  # Standard-Sonneneinstrahlung [W/m²]
+    beta_decimal = beta / 100.0 #Temperature coeficient in decimal
+    P = P_STC * (G / G_STC) * (1 - beta_decimal * (T_module - 25)) # Calculates Power in W
+    P = round(P, 1) #Rounds Power
+    return P
+
+def wind_power(rho, A, v, Cp):
+    """
+    Function to calculate the possible power of a wind turbine depending
+    on the inputs air density (rho), rotor swept area (A),
+    wind speed (v), and power coefficient (Cp)."""
+    P = 0.5 * rho * A * v**3 * Cp  # Calculates Power in W
+    P = round(P, 1) #Rounds Power
+    return P
+
+
+
