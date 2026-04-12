b1b09072f07309d13cd4837d0aa258e7b3225e72
Gregor Fischer <bajj17+github@pm.me>
2026-04-12 19:09:46 +0200

    Change test file and main file to check for linting and testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,14 +2,14 @@ from main import hydro_power, pv_power, wind_power
 
 # --- Tests für hydro_power ---
 def test_hydro_power():
-    assert hydro_power(0.9, 10, 50) == 4414.5
+    assert hydro_power(0.9, 10, 50) == 44.5
 
 
 # --- Tests für pv_power ---
 def test_pv_power():
-    assert pv_power(300, 800, 35, 0.4) == 230.4
+    assert pv_power(300, 800, 35, 0.4) == 23.4
 
 
 # --- Tests für wind_power ---
 def test_wind_power():
-    assert wind_power(1.225, 50, 10, 0.4) == 12250
+    assert wind_power(1.225, 50, 10, 0.4) == 1225
